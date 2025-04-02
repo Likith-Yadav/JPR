dfb5c9738f500f70984e9c88084375022772b287
Likith-Yadav <gnlikithyadav@gmail.com>
2025-04-02 20:29:11 +0530

    .

--- school_website/school_website/wsgi.py
+++ school_website/school_website/wsgi.py
@@ -8,8 +8,16 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
+from pathlib import Path
+from dotenv import load_dotenv
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+# Load environment variables from .env file
+env_path = BASE_DIR / '.env'
+load_dotenv(env_path)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'school_website.settings')
 
